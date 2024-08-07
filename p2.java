323a2013353a155ddb037898559d3a4aabc27595
krishanmurti <krishanmurti2003.in>
2024-08-07 22:03:36 +0530

    modified

--- p2.java
+++ p2.java
@@ -2,6 +2,7 @@ public class p2 {
     public static void main(String[] args) {
         System.out.println("welcome svcet");
         System.out.println("hi");
+        System.out.println("how are you");
     }
 
 }
